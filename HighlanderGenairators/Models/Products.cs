d16f7330bfac84339b9ffa007b88fc6872654b34
Zerovizboss <zeroviz.ddedman@yahoo.com>
2018-08-12 13:47:00 -0400

    Basic foundational changes to begin construction of HighlandsGenerators website

---
+++ HighlanderGenairators/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HighlanderGenairators.Models
+{
+    public class Products
+    {
+        public int GenId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Cost { get; set; }
+    }
+}
